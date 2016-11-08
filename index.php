2949a459ac1cd7e1878e8fc5fe7e847098da4c05
jshallow <jshallow@csumb.edu>
2016-11-08 04:56:57 +0000

    updated filter

--- index.php
+++ index.php
@@ -1,76 +1,79 @@
 <?php
 
+// to login
 $host = "localhost";
 $dbname = "laguna";
 $username = "web_user";
 $password = "s3cr3t";
 
-//Creates a database connection
+// Creates a database connection
 $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
 // Setting Errorhandling to Exception
 $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
 
-
 function getSearch()
 {
     global $dbConn;
+    
+    // get all records initially
     $sql = "SELECT * 
             FROM Make 
             NATURAL JOIN Model 
-            NATURAL JOIN CarType" ;  //Getting all records 
+            NATURAL JOIN CarType
+            WHERE 1" ;
             
-    if (isset($_GET['submit'])){
-    //form has been submitted
+    // search button pressed
+    if (isset($_GET['filter'])){
 
         $namedParameters = array();
         
-        
-        
+        // make has been specified
         if (!empty($_GET['make'])){
-            //deviceName has some value
-            
-            // Following sql works but it doesn't prevent SQL INJECTION
-           //  $sql = $sql . " AND deviceName LIKE  '%" . $_GET['deviceName'] . "%'";
-           $sql = $sql . " AND make LIKE  :deviceName "; //using Named Parameters to prevent SQL Injection
-           
-           $namedParameters[':make'] = "%" . $_GET['make'] . "%";
-           
+            //using Named Parameters to prevent SQL Injection
+            $sql = $sql . " AND make LIKE :make "; 
+            $namedParameters[':make'] = "%" . $_GET['make'] . "%";
         }
         
+        // model has been specified
         if(!empty($_GET['model'])){
-            //type has been selected
-            
-            $sql = $sql . " AND model = :model";
-            
-            $namedParameters[':model'] = $_GET['model'];
+            $sql = $sql . " AND model LIKE :model";
+            $namedParameters[':model'] = "%" . $_GET['model'] . "%";
         }
         
+        // car type is selected
         if(isset($_GET['cartype'])){
-            $sql = $sql . " AND cartype = :cartype";
-            $namedParameters[':cartype'] = "cartype";
+            $sql = $sql . " AND cartype LIKE :cartype";
+            $namedParameters[':cartype'] = "%" . $_GET['carType'] . "%";
         }
-        
-
-    
     }
             
-      $statement= $dbConn->prepare($sql); 
-      $statement->execute($namedParameters); //Always pass the named parameters, if any
-      $records = $statement->fetchALL(PDO::FETCH_ASSOC);  
-      
-      //print_r($records);
-      echo "<form action='shopping_cart.php' method=get>";
-      echo "<table border=1 cellpadding=20 >"; ;
-      echo "<tr><th cellpadding=20 cellspacing= 50 colspan=6>" . "Results". "</th></tr>";
-      foreach($records as $record) {
-          echo "<tr><td><input type='checkbox' name=cars[]    value =" . $record['model'] . "></td>";
-          echo "<td cellspacing=10 ><a target='DescriptioniFrame' href='description/".$record['model'].".html' > Description </a></td>";
-          echo "<td cellspacing=10 >" . $record['year'] . "</td><td cellspacing=10>" . $record['make'] . "</td><td cellpadding=20>". $record['model'] .  "</td><td cellspacing=10>". $record['cartype'] . "</td><tr>";
-      }
-      echo "<br></form>";
-   
+    $statement= $dbConn->prepare($sql); 
+    //Always pass the named parameters, if any
+    $statement->execute($namedParameters); 
+    $records = $statement->fetchALL(PDO::FETCH_ASSOC);  
     
+    echo "<form action='shopping_cart.php' method=get>";
+    echo "<table border=1 cellpadding=20 >"; ;
+    echo "<tr><th cellpadding=20 cellspacing= 50 colspan=6>" . "Results". "</th></tr>";
+      
+    // column headers
+    echo "<tr>";
+    echo "<td>Select</td>";
+    echo "<td>Description</td>";
+    echo "<td>Year</td>";
+    echo "<td>Make</td>";
+    echo "<td>Model</td>";
+    echo "<td>Type</td>";
+    echo "</tr>";
+    
+    // table data
+    foreach($records as $record) {
+        echo "<tr><td><input type='checkbox' name=cars[]    value =" . $record['model'] . "></td>";
+        echo "<td cellspacing=10 ><a target='DescriptioniFrame' href='description/".$record['modelId'].".html' > Description </a></td>";
+        echo "<td cellspacing=10 >" . $record['year'] . "</td><td cellspacing=10>" . $record['make'] . "</td><td cellpadding=20>". $record['model'] .  "</td><td cellspacing=10>". $record['cartype'] . "</td><tr>";
+    }
+    echo "<br></form>";
 }
 
 ?>
@@ -80,39 +83,27 @@ function getSearch()
 <html>
     <head>
         <title>Laguna Seca </title>
+        <link rel="stylesheet" href="css/style.css" type="text/css" />
     </head>
-    <style>
-        body,h1 {
-          text-align: center;  
-          margin: 0 auto;
-          background: url("mario.png");
-         
-        }
-    table
-    {
-                  text-align: center;  
-          margin: 0 auto;
-        background: white;
-    }
-    </style>
+    
     <body>
         <form method="get">
             <h1>Laguna Seca Race Way Rentals</h1>
             Make: <input type="text" maxlength="13" placeholder="Enter Make" name="make">
             Model: <input type="text" maxlength="10" placeholder="Enter Model" name="model">
             Type:
-            <select>
-                <option name="supercar" value="Super Car">Super Car</option>
-                <option name="luxury" value="Luxury">Luxury</option>
-                <option name="luxury" value="Economic">Economic</option>
-                <option name="off road" value="Off Road">Off Road</option>
+            <select name="type">
+                <option value="Super Car">Super Car</option>
+                <option value="Luxury">Luxury</option>
+                <option value="Economic">Economic</option>
+                <option value="Off Road">Off Road</option>
             </select>
             List By Type In:
-            <select>
-                <option name="asc" value="asc">Ascending</option>
-                <option name="desc" value="desc">Descending</option>
+            <select name="sort">
+                <option value="asc">Ascending</option>
+                <option value="desc">Descending</option>
             </select>
-            <input type="submit" value="Search">
+            <input type="submit" name="filter" value="Search">
             
         </form>    
         <div style="float:right">
@@ -122,7 +113,7 @@ function getSearch()
         <?php
         getSearch();
         ?>
-        <input type="submit" value="continue">
+        <input type="submit" name="reserve" value="Reserve">
         </form>
     </body>
     
